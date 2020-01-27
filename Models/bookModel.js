2f931a8339622a7f58b810ae52cf804f932d79c8
frederick <frederickbanks@icloud.com>
2020-01-26 19:01:20 -0500

    installed new dependicies/created .gitignore file/created bookSchema

---
+++ Models/bookModel.js
@@ -0,0 +1,13 @@
+// require connection file and assign it to mongoose variable
+const mongoose = require("../Database/connection");
+
+//Book Schema creation
+
+const bookSchema = new mongoose.Schema({
+  VolumeInfo: {
+    title: String,
+    Authors: []
+  }
+});
+
+module.exports = monggose.model("bookSchema", bookSchema);
